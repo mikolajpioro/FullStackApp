013f70af59f1fd0120be96b9eef1a40701586a4d
mikolajpioro <mytocpioro@gmail.com>
2026-03-29 16:47:03 +0200

    add error handling for HTTP exceptions and validation errors; update post template and styles for buttons

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, Request, HTTPException, status
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 import requests
 
 app = FastAPI()
@@ -49,6 +52,9 @@ def post_page(request: Request, post_id: int):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Couldn't find this post")
 
 
+
+
+
 @app.get("/api/posts")
 def get_posts():
     return posts
@@ -59,3 +65,31 @@ def get_post(post_id: int):
         if post.get("id") == post_id:
             return post
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Couldn't find this post")
+
+# Error handling -----------------------------------------
+@app.exception_handler(StarletteHTTPException)
+def general_http_exception_handler(request: Request, exception: StarletteHTTPException):
+    message = (
+        exception.detail
+        if exception.detail
+        else "An error has occured, Please check your request and try again."
+    )
+
+    if request.url.path.startswith("/api"):
+        return JSONResponse(
+            status_code=exception.status_code,
+            content={"detail": message},
+        )
+    return template.TemplateResponse(request,"error.html", {"status_code": exception.status_code, "title": exception.status_code, "message": message}, status_code=exception.status_code,)
+
+
+
+@app.exception_handler(RequestValidationError)
+def validation_exception(request: Request, execption: RequestValidationError):
+    if request.url.path.startswith("/api"):
+        return JSONResponse(
+            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+            content={"detail": execption.errors()},
+        )
+    
+    return template.TemplateResponse(request, "error.html", {"status_code": status.HTTP_422_UNPROCESSABLE_CONTENT, "title": status.HTTP_422_UNPROCESSABLE_CONTENT, "message": "Invalid request. Please check your input and try again."}, status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,)
